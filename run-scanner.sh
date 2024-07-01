fa32c89c79d284270bac2e6faeb80b0ba76295d2
Adrian Ferreres Esteller <adrian.steller@bbva.com>
2024-07-01 17:34:47 +0200

    feat: first version of action

---
+++ run-scanner.sh
@@ -0,0 +1,45 @@
+echo "Cleanning work directory"
+if test -f "./package-lock.json"; then
+    rm package-lock.json
+fi
+
+if test -f "./.npmrc"; then
+    rm .npmrc
+fi
+
+echo "Installing dependencies"
+npm i
+
+echo "Running test"
+npm run test
+
+echo "Running scanner"
+While [[ $# -gt 0 ]]; do
+    case "$1" in
+        --workspace)
+            WORKSPACE=$2
+            shift
+            shift
+            ;;
+        --token)
+            TOKEN=$2
+            shift
+            shift
+            ;;
+        --host)
+            HOST=$2
+            shift
+            shift
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+
+docker run -v $WORKSPACE:/usr/src -e SONAR_TOKEN=$TOKEN -e SONAR_HOST_URL=$HOST sonarsource/sonar-scanner-cli
